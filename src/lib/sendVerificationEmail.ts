3ab4ed6a388d0b803946e0315fd8a2e9ca92da72
Abdul Moid <abdulmoid337@gmail.com>
2024-12-15 03:18:58 +0530

    second

--- src/lib/sendVerificationEmail.ts
+++ src/lib/sendVerificationEmail.ts
@@ -1,6 +1,6 @@
 import { resend } from "./resend";
 import VerificationEmail from "../../emails/VerificationEmail";
-import { ApiResponse } from "@/types/ApiResponce";
+import { ApiResponse } from "@/types/ApiResponse";
 
 export async function sendVerificationEmail(
     email : string[],
