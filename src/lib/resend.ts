75a07a27d8d56234ed16d37bdd123f592fac5db9
Abdul Moid <abdulmoid337@gmail.com>
2024-12-15 19:52:36 +0530

    last

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,9 +1,3 @@
 import { Resend } from "resend";
 
-const resendApiKey = process.env.RESEND_API_KEY;
-
-if (!resendApiKey) {
-  throw new Error("Missing API key. Pass it to the constructor `new Resend('your-api-key')`");
-}
-
-export const resend = new Resend(resendApiKey);
+export const resend = new Resend(process.env.RESEND_API_KEY);
